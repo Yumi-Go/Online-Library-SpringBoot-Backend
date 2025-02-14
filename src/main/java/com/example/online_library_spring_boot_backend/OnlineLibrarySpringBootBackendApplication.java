86e495c9fb4a3a56d45b01d2d990dadf63a77cc7
Yumi Go <yumigoyumi@gmail.com>
2025-02-14 09:47:13 +0000

    Initial commit

---
+++ src/main/java/com/example/online_library_spring_boot_backend/OnlineLibrarySpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_library_spring_boot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibrarySpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibrarySpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_library_spring_boot_backend/OnlineLibrarySpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_library_spring_boot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibrarySpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
